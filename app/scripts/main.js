65ba472d8b19a6fb3c0b8c0e6505147e47152519
Shane Sniteman <ssniteman@gmail.com>
2014-02-11 23:21:38 -0500

    added array in javascript and working on remove button

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,29 @@
+var todoList = [
+{
+	userinput: 'Drink Wine',
+	done: false,
+	id: _.uniqueId()
+},
+{
+	userinput: 'Wash Shits',
+	done: false,
+	id: _.uniqueId()
+},
+{
+	userinput: 'Go To Da Bitch',
+	done: false,
+	id: _.uniqueId()
+}
+]
+
+
 var todoTemplate = _.template($('.todo-template').text())
 
+_.each(todoList, function(items){
+  $('.todo-items').prepend(todoTemplate(items));
+})
+
+// Add button
 
 $('.js-btn').click(function(){
 
@@ -15,3 +39,16 @@ var renderedTemplate = todoTemplate(todo);
 
 $('.todo-items').prepend(renderedTemplate)
 })
+
+// Remove button
+
+$('.todo-items').on('click', ".removebtn", function(){
+
+var 
+
+}
+
+
+
+
+
