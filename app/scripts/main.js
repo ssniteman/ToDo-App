89a3a93aa507329b7de96f4f8ddfe8dcb141c829
Shane Sniteman <ssniteman@gmail.com>
2014-03-09 17:18:50 -0400

    changed to standard todo instead of maria's

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,20 +1,16 @@
-var todoList = [
-{
-	userinput: 'Drink Wine',
-	done: false,
-	id: _.uniqueId('todo')
-},
-{
-	userinput: 'Wash Shits',
-	done: false,
-	id: _.uniqueId('todo')
-},
-{
-	userinput: 'Go To Da Bitch',
-	done: false,
-	id: _.uniqueId('todo')
-}
-];
+var todoList = [{
+    userinput: 'Feed Dog',
+    done: false,
+    id: _.uniqueId('todo')
+}, {
+    userinput: 'Finish Taxes',
+    done: false,
+    id: _.uniqueId('todo')
+}, {
+    userinput: 'Wash Clothes',
+    done: false,
+    id: _.uniqueId('todo')
+}];
 
 
 var todoTemplate = _.template($('.todo-template').text());
@@ -22,37 +18,37 @@ var todoTemplate = _.template($('.todo-template').text());
 
 // Adding pre objects to DOM
 
-_.each(todoList, function(items){
-  $('.todo-items').prepend(todoTemplate(items));
+_.each(todoList, function(items) {
+    $('.todo-items').prepend(todoTemplate(items));
 })
 
 // Add button
 
-$('.js-btn').click(function(){
+$('.js-btn').click(function() {
 
-	var userinput = $('.todo-input').val();
+    var userinput = $('.todo-input').val();
 
-	var todo = {
-	  userinput: userinput,
-	  done: false,
-	  id: _.uniqueId('todo'),
-	}
+    var todo = {
+        userinput: userinput,
+        done: false,
+        id: _.uniqueId('todo'),
+    }
 
-var renderedTemplate = todoTemplate(todo);
+    var renderedTemplate = todoTemplate(todo);
 
-console.log(todoTemplate(todo))
+    console.log(todoTemplate(todo))
 
-$('.todo-items').prepend(renderedTemplate)
+    $('.todo-items').prepend(renderedTemplate)
 
-// Add to the array
+    // Add to the array
 
-todoList.push(todo);
+    todoList.push(todo);
 
-console.log("New Todo List", todoList)
- 
- // counter tracker
+    console.log("New Todo List", todoList)
 
-$('.num').text(todoList.length);
+    // counter tracker
+
+    $('.num').text(todoList.length);
 
 
 });
@@ -62,28 +58,28 @@ $('.num').text(todoList.length);
 
 // Remove button
 
-$('.todo-items').on('click', '.removebtn', function(){
-	
-	var parentId = $(this).parents('.todo-item').attr('id');
+$('.todo-items').on('click', '.removebtn', function() {
 
-	console.log(parentId)
+    var parentId = $(this).parents('.todo-item').attr('id');
 
-	
-
-	todoList = _.reject(todoList, function(item){
-
-	return item.id == parentId;
-
-	})
-
-	console.log(todoList)
+    console.log(parentId)
 
 
-	$(this).parents('.todo-item').remove(); 
 
-// counter tracker
+    todoList = _.reject(todoList, function(item) {
 
-	$('.num').text(todoList.length);
+        return item.id == parentId;
+
+    })
+
+    console.log(todoList)
+
+
+    $(this).parents('.todo-item').remove();
+
+    // counter tracker
+
+    $('.num').text(todoList.length);
 
 
 });
@@ -100,7 +96,7 @@ $('.todo-items').on('click', '.removebtn', function(){
 //     $(this).parent().toggleClass('complete-item');
 
 //     var parentId = $(this).parent().attr('id').split('-')[1];
-    
+
 //     _.each(todoList, function(item,index){
 //       if(item.id == parentId) {
 //         item.done = true;
@@ -111,51 +107,55 @@ $('.todo-items').on('click', '.removebtn', function(){
 
 // completed task code
 
-	$('.todo-items').on('click', '.completebtn', function(){
-	
-		$(this).parent().siblings('.description').toggleClass('completed');
+$('.todo-items').on('click', '.completebtn', function() {
 
-		console.log('completed occurred')
+    $(this).parent().siblings('.description').toggleClass('completed');
 
-		
-		var parentId = $(this).parents('.todo-item').attr('id');
-		
-		console.log(parentId);
+    console.log('completed occurred')
 
 
-		var item = _.findWhere(todoList, {id: parentId});
-		
-		var toggleDone = !item.done;
+    var parentId = $(this).parents('.todo-item').attr('id');
 
-		item.done = toggleDone;
-		
-		console.log(item.done)
-
-// counter tracker
-
-		var completedItems = _.where(todoList, {done: true}); 
-		console.log(completedItems)
-
-		$('.num-two').text(completedItems.length);
-
-		
-		// +todoList.length;
-		// $('.num-two').text(todoList.length);
-
-		// var parentId = $(this).parent('.new-task-item').attr('id');
-
-		// var items = _.findWhere(taskList, {id: parentId});
+    console.log(parentId);
 
 
-		// items.done =!items.done;
+    var item = _.findWhere(todoList, {
+        id: parentId
+    });
 
-		// changeDone = items.done;
+    var toggleDone = !item.done;
 
-		// $(this).siblings().removeClass('false','true')
-		// $(this).siblings().addClass(changeDone.toString())
+    item.done = toggleDone;
 
-		// console.log(taskList)
-	});
+    console.log(item.done)
+
+    // counter tracker
+
+    var completedItems = _.where(todoList, {
+        done: true
+    });
+    console.log(completedItems)
+
+    $('.num-two').text(completedItems.length);
+
+
+    // +todoList.length;
+    // $('.num-two').text(todoList.length);
+
+    // var parentId = $(this).parent('.new-task-item').attr('id');
+
+    // var items = _.findWhere(taskList, {id: parentId});
+
+
+    // items.done =!items.done;
+
+    // changeDone = items.done;
+
+    // $(this).siblings().removeClass('false','true')
+    // $(this).siblings().addClass(changeDone.toString())
+
+    // console.log(taskList)
+});
 
 
 
@@ -166,37 +166,31 @@ $('.todo-items').on('click', '.removebtn', function(){
 
 
 
-$('.todo-items').on('click', '.editbtn', function(){
+$('.todo-items').on('click', '.editbtn', function() {
 
-	console.log("edit button clicked")
+    console.log("edit button clicked")
 
-	$(this).parent().siblings('.edit-input').show();
+    $(this).parent().siblings('.edit-input').show();
 
-	$(this).parent().siblings('.edit-input').focus();
+    $(this).parent().siblings('.edit-input').focus();
 
 
 });
 
-$('.todo-items').on('blur', '.edit-input', function(){
-    
+$('.todo-items').on('blur', '.edit-input', function() {
+
     var parentId = $(this).parent().attr('id');
-    
+
     var newDescription = $(this).val();
-	
-		var item = _.findWhere(todoList, {id: parentId});
 
-		item.userinput = newDescription 
+    var item = _.findWhere(todoList, {
+        id: parentId
+    });
 
-		$(this).siblings('.description').empty().html(newDescription)
-		
-		$(this).hide()
+    item.userinput = newDescription
+
+    $(this).siblings('.description').empty().html(newDescription)
+
+    $(this).hide()
 
 });
-
-
-
-
-
-
-
-
